3e5bf790e8f5720036ecfa3a4d830ed8c721f15d
Mehmet Akcakoca <akcakocamehmet@gmail.com>
2015-08-20 17:31:51 +0300

    Trimmed the branch to only the im_msgs sub directory

--- sdk/doxygen/en/html/search/classes_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['um6_5fdata',['UM6_DATA',['../structIMUM6_1_1UM6__DATA.html',1,'IMUM6']]],
-  ['union32',['UNION32',['../unionIMUM6_1_1UNION32.html',1,'IMUM6']]]
-];
--- sdk/doxygen/tr/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['exportgpio',['ExportGpio',['../classIMGPIO.html#a5a1bac8ae076039097a9de08d7efae38',1,'IMGPIO']]]
-];
