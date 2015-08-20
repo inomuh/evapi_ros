eaa35824727435c818445e89a3db61174ebe0b0a
Mehmet Akcakoca <akcakocamehmet@gmail.com>
2015-08-20 17:31:49 +0300

    Trimmed the branch to only the evarobot_infrared sub directory

--- sdk/doxygen/tr/html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['registers',['REGISTERS',['../structIMUM6_1_1REGISTERS.html',1,'IMUM6']]]
-];
--- sdk/doxygen/tr/html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mapregisteraddres',['MapRegisterAddres',['../classIMPWM.html#ab4967bd34c9cae505fcd7a176d89024d',1,'IMPWM']]]
-];
