94ef7d2c1007e2a8a4ff3b2cdbb02e1f6c6aa7d7
Mehmet Akcakoca <akcakocamehmet@gmail.com>
2015-11-27 16:09:53 +0200

    add diagnostics

--- evarobot_infrared/include/evarobot_infrared/evarobot_infrared.h
+++ evarobot_infrared/include/evarobot_infrared/evarobot_infrared.h
@@ -4,8 +4,16 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Range.h"
 
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/publisher.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <evarobot_infrared/ParamsConfig.h>
+
 #include <vector>
 #include <sstream>
+
+#include <boost/shared_ptr.hpp>
 
 #include "IMADC.h"
 
@@ -16,5 +24,56 @@
 #define MAX_IR 7
 
 using namespace std;
+
+class IMDynamicReconfig
+{
+	public:
+	IMDynamicReconfig();
+	
+	void CallbackReconfigure(evarobot_infrared::ParamsConfig &config, uint32_t level);
+	
+	ros::NodeHandle n_private;
+	
+	double d_min_range, d_max_range, d_fov;
+	double d_max_freq, d_min_freq;
+	double d_par_k, d_par_a, d_par_b;
+	
+	bool b_always_on;
+		
+	dynamic_reconfigure::Server<evarobot_infrared::ParamsConfig> srv;
+	dynamic_reconfigure::Server<evarobot_infrared::ParamsConfig>::CallbackType f;
+};
+
+class IMInfrared
+{
+	public:
+	
+	IMInfrared(int id,
+					boost::shared_ptr<IMADC> _adc,
+				  boost::shared_ptr<IMDynamicReconfig> _dynamic_params);
+	~IMInfrared();
+	bool ReadRange();
+	void Publish();
+	void ProduceDiagnostics(diagnostic_updater::DiagnosticStatusWrapper &stat);
+	
+	ros::NodeHandle n;
+  ros::Publisher pub_inf;
+  diagnostic_updater::Updater updater;
+  	
+	private:
+	
+	int i_id;
+	bool b_is_alive;
+	
+	sensor_msgs::Range inf_msg;
+	
+	boost::shared_ptr<IMADC> adc;
+	boost::shared_ptr<IMDynamicReconfig> dynamic_params;
+	
+	double min_freq;
+	double max_freq;
+	diagnostic_updater::HeaderlessTopicDiagnostic * pub_inf_freq;
+
+};
 
 #endif
--- evarobot_infrared/src/evarobot_infrared.cpp
+++ evarobot_infrared/src/evarobot_infrared.cpp
@@ -1,75 +1,177 @@
 #include "evarobot_infrared/evarobot_infrared.h"
 
-#include <dynamic_reconfigure/server.h>
-#include <evarobot_infrared/ParamsConfig.h>
+/*
+ * 
+ * IMInfrared
+ * 
+ * 
+ * */
 
-bool b_is_received_params = false;
-bool b_always_on;
-
-double g_d_fov;
-double g_d_min_range;
-double g_d_max_range;
-double g_d_par_k;
-double g_d_par_a;
-double g_d_par_b;
-
-void CallbackReconfigure(evarobot_infrared::ParamsConfig &config, uint32_t level)
+IMInfrared::IMInfrared(int id,
+											 boost::shared_ptr<IMADC> _adc,
+											 boost::shared_ptr<IMDynamicReconfig> _dynamic_params):i_id(id), adc(_adc), 
+													b_is_alive(false), dynamic_params(_dynamic_params)
 {
-   b_is_received_params = true;  
-         
-   b_always_on = config.alwaysOn;
-   g_d_fov = config.field_of_view;
-   g_d_min_range = config.minRange;
-   g_d_max_range = config.maxRange;
-   g_d_par_k = config.parK;
-   g_d_par_a = config.parA;
-   g_d_par_a = config.parB;
-   
+	stringstream ss;
+	
+	// Publisher
+	ss << "ir" << this->i_id;
+	this->pub_inf = this->n.advertise<sensor_msgs::Range>(ss.str().c_str(), 10);
+	
+	// Diagnostics
+	this->updater.setHardwareID("SHARP");
+	this->updater.add(ss.str().c_str(), this, &IMInfrared::ProduceDiagnostics);
+		
+	this->min_freq = this->dynamic_params->d_min_freq;
+	this->max_freq = this->dynamic_params->d_max_freq;
+	this->pub_inf_freq = new diagnostic_updater::HeaderlessTopicDiagnostic(ss.str().c_str(), this->updater,
+											diagnostic_updater::FrequencyStatusParam(&this->min_freq, &this->max_freq, 0.1, 10));
+											
+	
+	// Frame id
+	ss.str("");
+	ss << this->n.resolveName(n.getNamespace(), true) << "/ir" << this->i_id << "_link";
+			 	
+	this->inf_msg.header.frame_id = ss.str();
+	this->inf_msg.radiation_type = 1;
+	
 }
+
+IMInfrared::~IMInfrared()
+{
+	delete pub_inf_freq;
+}
+
+
+bool IMInfrared::ReadRange()
+{
+	int i_raw_data = 0;
+	bool b_ret = false;
+	float f_range = 0.0;
+			
+	i_raw_data = this->adc->ReadChannel(this->i_id);
+	f_range = 0.01 * ((1 / (this->dynamic_params->d_par_a * (double)i_raw_data + this->dynamic_params->d_par_b)) 
+						- this->dynamic_params->d_par_k);
+						
+	if(f_range > this->dynamic_params->d_max_range || f_range < 0)
+				f_range = this->dynamic_params->d_max_range;
+	
+
+	if(i_raw_data < 10)
+	{
+		this->b_is_alive = false;
+		b_ret = false;
+	}
+	else
+	{
+		this->b_is_alive = true;
+		b_ret = true;
+	}
+		
+	
+	this->inf_msg.range = f_range;
+	this->inf_msg.header.stamp = ros::Time::now();
+	this->inf_msg.field_of_view = this->dynamic_params->d_fov;
+	this->inf_msg.min_range = this->dynamic_params->d_min_range;
+	this->inf_msg.max_range = this->dynamic_params->d_max_range;
+	
+	return b_ret;
+}
+
+void IMInfrared::Publish()
+{
+	if(this->pub_inf.getNumSubscribers() > 0 || this->dynamic_params->b_always_on)
+	{
+		this->pub_inf.publish(this->inf_msg);
+		this->pub_inf_freq->tick();
+	}
+}
+
+void IMInfrared::ProduceDiagnostics(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+	if(this->b_is_alive)
+	{
+		stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "INFRARED%d is alive!", this->i_id);
+	}
+	else
+	{
+		stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "INFRARED%d is NOT alive!", this->i_id);
+	}
+}
+
+
+
+/*
+ * 
+ * IMDynamicReconfig
+ * 
+ * 
+ * */
+void IMDynamicReconfig::CallbackReconfigure(evarobot_infrared::ParamsConfig &config, uint32_t level)
+{
+	ROS_INFO("Updating Sonar Params...");
+		
+	this->d_fov = config.field_of_view;
+	this->d_min_range = config.minRange;
+	this->d_max_range = config.maxRange;
+	this->b_always_on = config.alwaysOn;
+	this->d_par_k = config.parK;
+	this->d_par_a = config.parA;
+	this->d_par_b = config.parB;
+}
+
+IMDynamicReconfig::IMDynamicReconfig():n_private("~")
+{
+	// Get Params
+	n_private.param("alwaysOn", this->b_always_on, false);
+	n_private.param("field_of_view", this->d_fov, 0.7);
+	n_private.param("minRange", this->d_min_range, 0.0);
+	n_private.param("maxRange", this->d_max_range, 5.0);
+	n_private.param("maxFreq", this->d_max_freq, 10.0);
+	n_private.param("minFreq", this->d_min_freq, 0.2);
+	n_private.param("parK", this->d_par_k, 0.42);
+	n_private.param("parA", this->d_par_a, 0.0000473);
+	n_private.param("parB", this->d_par_b, -0.0013461);
+		
+	ROS_DEBUG("b_always_on: %d", this->b_always_on);
+	ROS_DEBUG("d_fov: %f", this->d_fov);
+	ROS_DEBUG("d_min_range: %f", this->d_min_range);
+	ROS_DEBUG("d_max_range: %f", this->d_max_range);
+	
+	
+	// Dynamic Reconfigure
+	this->f = boost::bind(&IMDynamicReconfig::CallbackReconfigure, this, _1, _2);
+	this->srv.setCallback(this->f);
+}
+
 
 int main(int argc, char **argv)
 {
 	unsigned char u_c_spi_mode;
+	vector<IMInfrared *> infrared;
 	
 	// ROS PARAMS
 	vector<int> T_i_channels;
-	vector<double> T_d_posX, T_d_posY, T_d_posZ, T_d_angle_yaw;
-	vector<double> T_d_min_ranges, T_d_max_ranges;
-	vector<double> T_d_field_of_views;
-	
-	vector<double> T_d_par_k, T_d_par_a, T_d_par_b;
-		
 	double d_frequency;
 	
 	string str_driver_path;
-	string str_namespace;
-	string str_frame_id;
 	
 	int i_spi_mode;
 	int i_spi_speed;
 	int i_spi_bits;
 	int i_adc_bits;
-	
-	
 	// rosparams end
 	
-	vector<ros::Publisher> T_pub_ir;
-	vector<sensor_msgs::Range> T_irs;
 	
-	ros::init(argc, argv, "evarobot_infrared");
+	ros::init(argc, argv, "/evarobot_infrared");
 	ros::NodeHandle n;
 	
 	n.param<string>("evarobot_infrared/driverPath", str_driver_path, "/dev/spidev0.0");
-	n.param<string>("evarobot_infrared/namespace", str_namespace, "gazebo");
-	n.param<string>("evarobot_infrared/frameNamespace", str_frame_id, "infrared");
-	n.param("evarobot_infrared/alwaysOn", b_always_on, false);
 	n.param("evarobot_infrared/spiMode", i_spi_mode, 0);
 	n.param("evarobot_infrared/spiSpeed", i_spi_speed, 1000000);
 	n.param("evarobot_infrared/spiBits", i_spi_bits, 8);
 	n.param("evarobot_infrared/adcBits", i_adc_bits, 12);
-	
-
-		
+			
 	if(!n.getParam("evarobot_infrared/frequency", d_frequency))
 	{
 		ROS_ERROR("Failed to get param 'frequency'");
@@ -80,88 +182,19 @@ int main(int argc, char **argv)
 		ROS_ERROR("adcBits mustn't be greater than 16");
 	}
 	
-	n.getParam("evarobot_infrared/parK", T_d_par_k); // 0.42
-	n.getParam("evarobot_infrared/parB", T_d_par_b); // -0.0013461
-	n.getParam("evarobot_infrared/parA", T_d_par_a); // 0.0000473
-
-	n.getParam("evarobot_infrared/field_of_view", T_d_field_of_views);
-	n.getParam("evarobot_infrared/minRange", T_d_min_ranges);
-	n.getParam("evarobot_infrared/maxRange", T_d_max_ranges);
-
 	n.getParam("evarobot_infrared/channelNo", T_i_channels);
-		
-	n.getParam("evarobot_infrared/posX", T_d_posX);
-	n.getParam("evarobot_infrared/posY", T_d_posY);
-	n.getParam("evarobot_infrared/posZ", T_d_posZ);
-	n.getParam("evarobot_infrared/angleYaw", T_d_angle_yaw);
-	
-	g_d_fov = T_d_field_of_views[0];
-  g_d_min_range = T_d_min_ranges[0];
-  g_d_max_range = T_d_max_ranges[0];
-  g_d_par_k = T_d_par_k[0];
-  g_d_par_a = T_d_par_a[0];
-  g_d_par_b = T_d_par_b[0];
-	
-	if( !(T_d_par_k.size() == T_d_par_a.size()
-	&& T_d_par_a.size() == T_d_par_b.size()
-	&& T_d_par_b.size() == T_d_field_of_views.size()
-	&& T_d_field_of_views.size() == T_d_min_ranges.size()
-	&& T_d_min_ranges.size() == T_d_max_ranges.size()
-	&& T_d_min_ranges.size() == T_i_channels.size()
-	&& T_i_channels.size() == T_d_posX.size()
-	&& T_d_posX.size() == T_d_posY.size()
-	&& T_d_posY.size() == T_d_posZ.size()
-	&& T_d_posZ.size() == T_d_angle_yaw.size())	)
-	{
-		ROS_ERROR("Size of params are not same.");
-	}
-	else if(T_i_channels.size() > MAX_IR)
+			
+	if(T_i_channels.size() > MAX_IR)
 	{
 		ROS_ERROR("Number of infrared sensors mustn't be greater than %d", MAX_IR);
 	}
 	
-	#ifdef DEBUG
-		ROS_INFO("Number of ir sensors: %d", T_d_posZ.size());
-	#endif
+	ROS_INFO("Number of ir sensors: %d", T_i_channels.size());
 	
-	// Dynamic Reconfigure
-	dynamic_reconfigure::Server<evarobot_infrared::ParamsConfig> srv;
-	dynamic_reconfigure::Server<evarobot_infrared::ParamsConfig>::CallbackType f;
-	f = boost::bind(&CallbackReconfigure, _1, _2);
-	srv.setCallback(f);
-	///////////////
 	
-	// Set publishers
-	for(uint i = 0; i < T_i_channels.size(); i++)
-	{
-		stringstream ss_topic;
-		ss_topic << str_namespace << "/ir" << i;
-		ros::Publisher dummy_publisher = n.advertise<sensor_msgs::Range>(ss_topic.str().c_str(), 10);
-		T_pub_ir.push_back(dummy_publisher);
-	}
-
 	// Define frequency
 	ros::Rate loop_rate(d_frequency);
 
-	
-	
-	// init sonar variables
-	for(uint i = 0; i < T_i_channels.size(); i++)
-	{
-		
-		stringstream ss_frame;
-		ss_frame << str_frame_id << "/ir" << i << "_link";
-		
-		sensor_msgs::Range dummy_ir;
-		
-		dummy_ir.header.frame_id = ss_frame.str();
-		dummy_ir.radiation_type = 1;
-		dummy_ir.field_of_view = T_d_field_of_views[i];
-		dummy_ir.min_range = T_d_min_ranges[i];
-		dummy_ir.max_range = T_d_max_ranges[i];
-		
-		T_irs.push_back(dummy_ir);
-	}
 	
 	switch(i_spi_mode)
 	{
@@ -195,63 +228,39 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	// Creating spi and adc objects.
-	IMSPI * p_im_spi = new IMSPI(str_driver_path, u_c_spi_mode, i_spi_speed, i_spi_bits);
-	IMADC * p_im_adc = new IMADC(p_im_spi, i_adc_bits);
+	// create objects
+	boost::shared_ptr<IMDynamicReconfig> dyn_params(new IMDynamicReconfig);
+	IMSPI * p_im_spi(new IMSPI(str_driver_path, u_c_spi_mode, i_spi_speed, i_spi_bits));
+	boost::shared_ptr<IMADC> p_im_adc(new IMADC(p_im_spi, i_adc_bits));
 	
+	for(uint i = 0; i < T_i_channels.size(); i++)
+	{
+		if(T_i_channels[i] < MAX_IR)
+		{
+			IMInfrared * dummy_inf = new IMInfrared(T_i_channels[i], p_im_adc, dyn_params);
+			infrared.push_back(dummy_inf);
+		}
+		else
+		{
+			ROS_ERROR("Undefined channel value.");
+		}
+	}
 	
 	while(ros::ok())
 	{
-		
-		if(b_is_received_params)
+		for(uint i = 0; i < T_i_channels.size(); i++)
 		{
-			ROS_INFO("Updating Infrared Params...");
-						
-	/*		for(uint i = 0; i < T_i_channels.size(); i++)
+			if(infrared[i]->ReadRange())
 			{
-				T_irs[i].field_of_view = g_d_fov;
-				T_irs[i].min_range = g_d_min_range;
-				T_irs[i].max_range = g_d_max_range;
-				T_d_par_a[i] = g_d_par_a;
-				T_d_par_b[i] = g_d_par_b;
-				T_d_par_k[i] = g_d_par_k;
-			}
-			*/
-			b_is_received_params = false;
-		}
-				
-		for(uint i = 0; i < T_pub_ir.size(); i++)
-		{
-			// Read sensors
-			int i_raw_data = 0;
+				infrared[i]->Publish();
+			}	
 			
-			i_raw_data = p_im_adc->ReadChannel(T_i_channels[i]);
-			ROS_DEBUG("raw data: ---- %i", i_raw_data);
-			#ifdef DEBUG
-				ROS_INFO("Raw data %f \n", (double)i_raw_data);
-				ROS_INFO("Par_A %f \n", T_d_par_a[i]);
-				ROS_INFO("Par_B %f \n", T_d_par_b[i]);
-				ROS_INFO("Par_K %f \n", T_d_par_k[i]);
-			#endif
-			
-			
-			T_irs[i].range = 0.01 * ((1 / (T_d_par_a[i] * (double)i_raw_data + T_d_par_b[i])) - T_d_par_k[i]);
-			T_irs[i].header.stamp = ros::Time::now();
-			
-			if(T_irs[i].range > T_irs[i].max_range || T_irs[i].range < 0)
-				T_irs[i].range = T_irs[i].max_range;
-								
-			// Publish Data
-			if(T_pub_ir[i].getNumSubscribers() > 0 || b_always_on)
-			{
-				T_pub_ir[i].publish(T_irs[i]);
-			}
+			infrared[i]->updater.update();
 			
 		}
 		
 		ros::spinOnce();
 		loop_rate.sleep();	
-	
 	}
 	
 	return 0;
